db87ba380ed52b1de521c4d215585924f018f7b2
Sarah Schneider <rifkegribenes@gmail.com>
2017-04-08 19:48:08 +0000

    alternate win function created; basic computer player added (incomplete)

--- script.js
+++ script.js
@@ -1,48 +1,63 @@
 $( document ).ready(function() {
     var player = 'X';
+    var computer = '';
     var cell;
     var cellValues = {};
     var winner;
     var xWins = 0;
     var oWins = 0;
 
+
+// pop-up box at beginning
+
+$('#chooseX').click(function(){
+	player = 'X';
+	$('#chooseX').css('background-color','#ccc');
+	$('#chooseO').css('background-color', '');
+});
+$('#chooseO').click(function(){
+	player = 'O';
+	$('#chooseO').css('background-color','#ccc');
+	$('#chooseX').css('background-color', '');
+});
+$('#human').click(function(){
+    $('#overlay').hide();
+});
+$('#computer').click(function(){
+    $('#overlay').hide();
+    computer = 1;
+});
+
+// choosing individual cells
 $('.cell').click(function(){
     cell = this.id;
     makeMove(cell);
+    $(this).removeClass("empty");
 });
 
-function makeMove(cell) {
+function makeMove(cell) { // populates board with player icon and updates record of available spaces
 
     var cellValue = document.getElementById(cell).innerHTML;
     if (cellValue == ''){
         document.getElementById(cell).innerHTML = player; // update cell with player token
         cellValues[`${cell}`] = player; // record values of cells already taken      
-        checkGameStatus();
-        if (player == 'X'){ // alternates players
-        player = 'O';
-        } else {
-        player = 'X';
-        } 
+        //checkGameStatus();
+        checkWin(player);
+        switchPlayers();
     } 
-    return player;
+    if (computer != ''){
+        computerMove();
+    }
 }
 
-// pop-up box at beginning
-
-$('#chooseX').click(function(){
-    $('#overlay').hide();
-	player = 'X';
-})
-$('#chooseO').click(function(){
-    $('#overlay').hide();
-	player = 'O';
-})
-
-// to do:
-// robin: populate cells, logging values of used cells (DONE)
-// sarah: figure out if game is won (DONE)
-// need a function to reset the game
-// need to call reset once game is won
+// change player turn
+function switchPlayers(){
+    if(player == 'X'){
+        player = 'O';
+    }else{
+        player = 'X';
+    }
+}
 
 function checkGameStatus() {
     
@@ -120,5 +135,153 @@ function resetGame() {
 }, (2000));
 }
 
+/*
+// display player icons on hover
+$('.empty').hover(function(){
+    $(this).html(`${player}`);
+    $(this).click(click());
+}, function(){
+    $(this).html('');
+});
+
+$('.empty').click(function(){	
+	$('.empty').unbind("hover");
+	});
+*/
+
+// new check win function
+function checkWin(player) {
+    if (
+         (cellValues[1] == player && cellValues[2] == player && cellValues[3] == player) ||
+         (cellValues[4] == player && cellValues[5] == player && cellValues[6] == player) ||
+         (cellValues[7] == player && cellValues[8] == player && cellValues[9] == player) ||
+         (cellValues[1] == player && cellValues[4] == player && cellValues[7] == player) ||
+         (cellValues[2] == player && cellValues[5] == player && cellValues[8] == player) ||
+         (cellValues[3] == player && cellValues[6] == player && cellValues[9] == player) ||
+         (cellValues[1] == player && cellValues[5] == player && cellValues[9] == player) ||
+         (cellValues[3] == player && cellValues[5] == player && cellValues[7] == player)
+ ) {
+         winner = player;
+        $('.message').html(winner + " wins the game!");
+        updateScore(winner);
+        resetGame();
+ } else if( Object.keys(cellValues).length == 9) {
+        $('.message').html("it's a draw!");
+        resetGame();
+ }
+};
+
+// computer move with random selection
+var randomChoice;
+var choice;
+function computerMove(retry){
+    //var retry;
+    randomChoice =  Math.ceil(Math.random()*9);
+    choice = parseInt(randomChoice, 10);
+    console.log(choice);
+    function placeToken(){
+        if(cellValues.hasOwnProperty(choice)){
+            console.log('foo');
+        }else{
+            $('#' + choice).html(player);
+            if (player == 'X'){
+                player = 'O';
+            } else {
+                player = 'X';
+            }
+        }
+    }
+    placeToken();
+}
+$('#compy').click(function(){
+    computerMove();
+    console.log(randomChoice.valueOf());
+    console.log(choice);
+})
+
+
+/*
+// beginning of computer Minimax algorithm
+//check if given board results in win
+function winning(board, player){
+ if (
+        (board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player)
+        ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// check available spots
+function avail(reboard) {
+  return reboard.filter(s => s != "P" && s != "C");
+}
+
+var calls = 0;
+
+function minimax(reboard, player) {
+  calls++;
+  let array = avail(reboard);
+  if (winning(reboard, huPlayer)) {
+    return {
+      score: -10
+    };
+  } else if (winning(reboard, aiPlayer)) {
+    return {
+      score: 10
+    };
+  } else if (array.length === 0) {
+    return {
+      score: 0
+    };
+  }
+
+  var moves = [];
+  for (var i = 0; i < array.length; i++) {
+    var move = {};
+    move.index = reboard[array[i]];
+    reboard[array[i]] = player;
+
+    if (player == aiPlayer) {
+      var g = minimax(reboard, huPlayer);
+      move.score = g.score;
+    } else {
+      var g = minimax(reboard, aiPlayer);
+      move.score = g.score;
+    }
+    reboard[array[i]] = move.index;
+    moves.push(move);
+  }
+
+  var bestMove;
+  if (player === aiPlayer) {
+    var bestScore = -10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    var bestScore = 10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+  return moves[bestMove];
+}
+
+*/
 
 }); //end of document.ready
